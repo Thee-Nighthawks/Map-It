f91a6baa026c706c21bdafb11098cecb8bd15dcb
tejas-gk <tejgankri@outlook.com>
2022-10-30 15:38:43 +0530

    flyto added

--- map/src/App.jsx
+++ map/src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css"
 import GoogleMapsReact from 'google-map-react'
 import TryMap from "./components/TryMap";
-
+import useGeoLocation from "./hooks/useGeoLocation";
 function App() {
   const coordinates = {
     lat: 59.95,
     lng: 30.33
   }
+
   return (
     <div className="App">
        <h1>some project</h1>
-       
+          
           <TryMap />
          </div>
          
--- map/src/components/TryMap.jsx
+++ map/src/components/TryMap.jsx
@@ -1,5 +1,6 @@
 import {MapContainer,TileLayer,Marker} from 'react-leaflet'
 import {useState,useRef} from 'react'
+import L from 'leaflet'
 import osm from '../app/osm-providers'
 import 'leaflet/dist/leaflet.css'
 import useGeoLocation from '../hooks/useGeoLocation'
@@ -7,9 +8,34 @@ export default function TryMap() {
     const [center,setCenter] = useState({lat: 59.95,lng: 30.33})
 
     const mapRef = useRef()
-    const ZOOM_LEVEL=9
+    const ZOOM_LEVEL=16
     const position = [51.505, -0.09] // test purpose
     const location=useGeoLocation()
+
+    //  changable values
+    const markerIcon = L.icon({
+        iconUrl: 'https://img.icons8.com/ios-glyphs/344/location-off.png',
+        iconSize: [38, 95],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76],
+        shadowSize: [68, 95],
+        shadowAnchor: [22, 94]
+    })
+
+    const handleClick = (e) => {
+        if(location.loaded && !location.error){
+          console.log('your location is',location.coordinates)
+            mapRef.current.flyTo([location.coordinates.lat,location.coordinates.lng],
+                ZOOM_LEVEL,
+                {animate:true}
+            )
+          
+
+            
+        }else{
+          console.log("not loaded")
+        }
+      }
 
 
     return(
@@ -20,11 +46,11 @@ export default function TryMap() {
             scrollWheelZoom={true}
             style={{height:'100vh',width:'100%'}}
             ref={mapRef}
+            whenCreated={handleClick}
             >
                 <TileLayer
                     url={osm.maptiler.url}
                     attribution={osm.maptiler.attribution}
-
                 />
                 {location.loaded && !location.error && (
                     <Marker 
@@ -33,7 +59,10 @@ export default function TryMap() {
                     />
                 )}
             </MapContainer>
-
+          
+            <button
+            onClick={handleClick}
+          >locate me</button>
         </div>
     )
 }
