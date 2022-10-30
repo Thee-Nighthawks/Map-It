717e69f5e05f200f5f1e30d38f713b80cc6ae367
AkKiL <78695802+HeimanPictures@users.noreply.github.com>
2022-10-30 15:09:26 +0530

    Added Redis

---
+++ map/src/app/redis/redis.js
@@ -0,0 +1,71 @@
+class redis {
+    constructor(hostname, year) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    add(password) {
+        /**
+         * password: string
+         */
+        const redis = require('redis');
+        const client = redis.createClient({
+            socket: {
+                host: this.hostname,
+                port: this.port
+            },
+            password: password
+        });
+        client.on('error', err => {
+            console.log('Error ' + err);
+            return false
+        });
+
+        this.client = client
+        return true
+    }
+
+    set(userId, lat, lon) {
+        /*
+        userId: string
+        lat: string 
+        lon: string
+        */
+        this.client.hmset(user, {
+            'lat': lat,
+            'lon': lon
+        }, function(err, reply) {
+            return true
+        });
+        return false
+    }
+
+    get(value) {
+        /*
+        value: string
+        */
+        client.exists(value, function(err, reply) {
+            if (reply === 1) {
+                this.client.get(value, function(err, vue) {
+                    return true, vue
+                });
+            } else {
+                return false, null
+            }
+        })
+    }
+
+    delete(value) {
+        /*
+        value: string
+        */
+        client.exists(value, function(err, reply) {
+            if (reply === 1) {
+                redis.del(value)
+                return true
+            } else {
+                return false
+            }
+        })
+    }
+}
