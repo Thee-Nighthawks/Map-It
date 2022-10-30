8fc698758422c4ecedb8be960e8a4e8e9a7c8c6e
Deveesh Shetty <deveeshshetty@gmail.com>
2022-10-30 19:45:44 +0530

    Dashboard Changes

--- map/src/components/Dashboard/Dashboard.jsx
+++ map/src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,10 @@
-import config from "../../app/config"
 import "./Dashboard.css"
 
 function Dashboard() {
   return (
     <div className="wrapper">
       <div>
-        <h1>Notification</h1>
+        <h1 style={{ color: "var(--clr-primary-400)" }}>Notification</h1>
         <div className="user-dashboard">
           <div>
             <h2>Location</h2>
@@ -15,9 +14,46 @@ function Dashboard() {
             </div>
             <p>City</p>
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>
+            <h2>Location</h2>
+            <div>
+              <p>Town</p>
+              <p>Address</p>
+            </div>
+            <p>City</p>
+          </div>
+          <div>
+            <h2>Location</h2>
+            <div>
+              <p>Town</p>
+              <p>Address</p>
+            </div>
+            <p>City</p>
+          </div>
+          <div>
+            <h2>Location</h2>
+            <div>
+              <p>Town</p>
+              <p>Address</p>
+            </div>
+            <p>City</p>
+          </div>
+          <div>
+            <h2>Location</h2>
+            <div>
+              <p>Town</p>
+              <p>Address</p>
+            </div>
+            <p>City</p>
+          </div>
+          <div>
+            <h2>Location</h2>
+            <div>
+              <p>Town</p>
+              <p>Address</p>
+            </div>
+            <p>City</p>
+          </div>
         </div>
       </div>
       <div>
