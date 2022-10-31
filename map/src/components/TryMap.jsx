e297d5fdd08ba2d4d136729fd3ef9f6b4926a514
tejas-gk <tejgankri@outlook.com>
2022-10-31 05:56:59 +0530

    .

--- map/src/components/TryMap.jsx
+++ map/src/components/TryMap.jsx
@@ -84,7 +84,9 @@ export default function TryMap({ coordinates, setCoordinates }) {
       getShortestPath(latLng)
       createCircle(latLng)
       userExixtsInsideCircle(latLng)
-
+      for(let i=0;i<10;i++)
+      put100Markers()
+      getAllMarkersInCircle (latLng)
   }
   const createCircle = (e) => {
     L.circle([e.lat, e.lng], {
@@ -109,19 +111,32 @@ export default function TryMap({ coordinates, setCoordinates }) {
     }
   }
 
-  const getCoordinatesOfMarker = (e) => {
-    // console.log("marker", e.target._latlng)
-    let latLng = e.target._latlng
-    const geodingUrl = `https://api.opencagedata.com/geocode/v1/json?q=${latLng.lat}+${latLng.lng}&key=1c7e646c300b43d4a8c16a1a3d7e0d70`
-    fetch(geodingUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log("data", data)
-        // marker.bindPopup(data.results[0].formatted).openPopup()
-        // setGetLocationName(data.results[0].formatted)
-        console.log("Location", data.results[0])
-        setCoordinates(data.results[0].components)
-      })
+  const getAllMarkersInCircle = (e) => {
+    let markers = mapRef.current._layers
+    let circleLocation = [e.lat, e.lng]
+    let markersInCircle = []
+    for (let i in markers) {
+      if (markers[i].options.icon) {
+        let markerLocation = [markers[i]._latlng.lat, markers[i]._latlng.lng]
+        let distance = mapRef.current.distance(markerLocation, circleLocation)
+        // if (distance <= radius) {
+          markersInCircle.push(markers[i])
+        // }
+      }
+    }
+    console.log("markersInCircle", markersInCircle)
+  }
+
+  const randomnumber = (number) => {
+    return Math.floor(Math.random() * number)
+  }
+
+  const put100Markers = () => {
+   
+     L.marker([position[0] + randomnumber(100) / 100000, position[1] + randomnumber(100) / 100000], {
+        icon: markerIcon,
+      }).addTo(mapRef.current)
+    
   }
 
   const getShortestPath = (e) => {
